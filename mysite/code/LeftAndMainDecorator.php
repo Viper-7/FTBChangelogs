6dcb175a376cc8f0668fd91e05ff380f9e853ee2
viper-7 <viper7@viper-7.com>
2015-03-06 21:46:26 +1100

    Fixed whitespace

--- mysite/code/LeftAndMainDecorator.php
+++ mysite/code/LeftAndMainDecorator.php
@@ -1,10 +1,9 @@
 <?php
-class LeftAndMainDecorator extends Extension
-{
+class LeftAndMainDecorator extends Extension {
     public function onAfterInit() {
-	CMSMenu::remove_menu_item('Help');
-	CMSMenu::remove_menu_item('ReportAdmin');
-	CMSMenu::remove_menu_item('CMSPagesController');
-	CMSMenu::remove_menu_item('CMSSettingsController');
+		CMSMenu::remove_menu_item('Help');
+		CMSMenu::remove_menu_item('ReportAdmin');
+		CMSMenu::remove_menu_item('CMSPagesController');
+		CMSMenu::remove_menu_item('CMSSettingsController');
     }
 }
