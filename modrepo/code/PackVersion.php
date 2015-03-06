5503dd20d3e03f899c71ae3ab4105e5497498390
viper-7 <viper7@viper-7.com>
2015-03-07 01:52:15 +1100

    Use more aggressive Item cleanup renaming

--- modrepo/code/PackVersion.php
+++ modrepo/code/PackVersion.php
@@ -140,8 +140,10 @@ class PackVersion extends DataObject {
 					$mod->write();
 				}
 
-				if($name == '' || $name == $internal || $name == 'null' || trim($name) == 'Name could not be retrieved due to an error: java.lang.NullPointerException')
-					$name = ucwords(preg_replace('/^<(.+)>$|^(.+)\|.+?$/m', '$1', str_replace(array('.', '<', '>'), ' ', str_replace(array('tile.block', 'tile.', '.name'),'',$internal))));
+				if($name == '' || $name == 'null' || trim($name) == 'Name could not be retrieved due to an error: java.lang.NullPointerException')
+					$name = $internal;
+				
+				$name = ucwords(preg_replace('/^<(.+)>$|^(.+)\|.+?$/m', '$1', str_replace(array('.', '<', '>'), ' ', str_replace(array('tile.block', 'tile.', '.name'), '', $internal))));
 
 				$item = DataObject::get_one('Item', 'InternalName=\'' . Convert::raw2sql(trim($internal)) . '\' AND ModID=' . intval($mod->ID) . ' AND PackID=' . intval($this->PackID));
 				
