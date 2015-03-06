5babf19eb87dc873a79bd46fe99e0deaaef10291
viper-7 <viper7@viper-7.com>
2015-03-06 22:00:07 +1100

    Re-enabled site content management

--- mysite/code/LeftAndMainDecorator.php
+++ mysite/code/LeftAndMainDecorator.php
@@ -3,7 +3,6 @@ class LeftAndMainDecorator extends Extension {
     public function onAfterInit() {
 		CMSMenu::remove_menu_item('Help');
 		CMSMenu::remove_menu_item('ReportAdmin');
-		CMSMenu::remove_menu_item('CMSPagesController');
 		CMSMenu::remove_menu_item('CMSSettingsController');
     }
 }
